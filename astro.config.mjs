4ca0792f5bfe90ebcdc69da956e15d1b4d4a087f
Eldritch Elbow <482171+eldritch-elbow@users.noreply.github.com>
2026-07-07 15:26:08 +0100

    fix: BioUK plus SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// Deploying to https://eldritch-elbow.github.io/emergent-systems-website/ for now.
-// When a custom domain is ready: drop `base`, add a `public/CNAME` file with the domain.
 export default defineConfig({
-  site: 'https://emergentsystems.io'
+  site: 'https://emergentsystems.io',
+  integrations: [sitemap()],
 });
